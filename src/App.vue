dd56b08beb95a946b7d6b8acf0e2ab6c35833176
chenxuanquan <chenxuanquan123>
2021-01-04 18:30:31 +0800

    主分支stash

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
 <div>
   <div>我在试突</div>
   <div>我在弄冲突第二个</div>
-  <div>我新建分支dev</div>
+  <div>我现在在hello主分支</div>
 <el-menu :default-active="activeIndex" class="el-menu-demo" mode="horizontal" @select="handleSelect">
   <el-menu-item index="1">处理中心</el-menu-item>
   <el-submenu index="2">
